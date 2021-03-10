f95e4093ba2997dc47b6f115ec5a4b88b144c4a1
Gustavo Willemann <gustavowill@hotmail.com>
2021-03-10 15:07:59 -0300

    feat(seo): projeto ao final da aula 03 modulo 03

--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,17 @@ import { ThemeProvider } from 'styled-components';
 import Head from 'next/head';
 import GlobalStyle from '../src/theme/GloboStyle';
 import theme from '../src/theme';
+import SEO from '../src/components/commons/SEO';
 
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Intalura - Gustavowill</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet" />
       </Head>
+      <SEO headTitle="Home" />
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
