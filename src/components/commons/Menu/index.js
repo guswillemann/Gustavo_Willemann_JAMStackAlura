406f0eff05ea52ca0c16a346a506b4cd04857e55
Gustavo Willemann <gustavowill@hotmail.com>
2021-03-10 11:36:15 -0300

    feat: projeto ao final da aula 01 modulo 03

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Logo } from '../../../theme/Logo';
 import MenuWrapper from './styles/MenuWrapper';
 import Button from '../Button';
-import Text from '../../foundation/Text';
+import Link from '../Link';
 
 const links = [
   {
@@ -19,7 +20,7 @@ const links = [
   },
 ];
 
-export default function Menu() {
+export default function Menu({ onRegisterClick }) {
   return (
     <MenuWrapper>
       <MenuWrapper.LeftSide>
@@ -28,20 +29,24 @@ export default function Menu() {
       <MenuWrapper.Central>
         {links.map((link) => (
           <li key={link.url}>
-            <Text variant="smallestException" tag="a" href={link.url}>
+            <Link href={link.url}>
               {link.texto}
-            </Text>
+            </Link>
           </li>
         ))}
       </MenuWrapper.Central>
       <MenuWrapper.RightSide>
-        <Button ghost variant="secondary.main">
+        <Button ghost variant="secondary.main" href="/app/login">
           Entrar
         </Button>
-        <Button variant="primary.main">
+        <Button variant="primary.main" onClick={onRegisterClick}>
           Cadastrar
         </Button>
       </MenuWrapper.RightSide>
     </MenuWrapper>
   );
 }
+
+Menu.propTypes = {
+  onRegisterClick: PropTypes.func.isRequired,
+};
