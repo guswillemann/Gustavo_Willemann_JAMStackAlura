cad734c561347d027e8614aba4c91f39c2f515a1
Gustavo Willemann <gustavowill@hotmail.com>
2021-03-30 16:06:36 -0300

    feat: projeto ao final da aula 03 modulo 04

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -1,20 +1,26 @@
 /// <reference types="cypress" />
 
+import LoginScreenPageObject from '../../../../src/components/screens/app/LoginScreen/LoginScreen.pageObject';
+
 describe('/pages/app/login/', () => {
-  it('fills the imputs and access /app/profile', () => {
-    cy.intercept('https://instalura-api-git-master-omariosouto.vercel.app/api/login')
-      .as('userLogin');
-    cy.visit('/app/login/');
-    cy.get('#formLogin input[name="user"]').type('omariosouto');
-    cy.get('#formLogin input[name="password"]').type('senhasegura');
-    cy.get('#formLogin button[type="submit"]').click();
-    cy.url().should('include', '/app/profile');
-    cy.wait('@userLogin')
-      .then((intercept) => {
-        const { token } = intercept.response.body.data;
-        cy.getCookie('APP_TOKEN')
-          .should('exist')
-          .should('have.property', 'value', token);
-      });
+  describe('when fill and submit a form login request', () => {
+    it('fills the inputs and access /app/profile', () => {
+      cy.intercept('https://instalura-api-git-master-omariosouto.vercel.app/api/login')
+        .as('userLogin');
+
+      const loginScreen = new LoginScreenPageObject(cy);
+      loginScreen
+        .fillLoginForm({ user: 'omariosouto', password: 'senhasegura' })
+        .submitLoginForm();
+
+      cy.url().should('include', '/app/profile');
+      cy.wait('@userLogin')
+        .then((intercept) => {
+          const { token } = intercept.response.body.data;
+          cy.getCookie('APP_TOKEN')
+            .should('exist')
+            .should('have.property', 'value', token);
+        });
+    });
   });
 });
