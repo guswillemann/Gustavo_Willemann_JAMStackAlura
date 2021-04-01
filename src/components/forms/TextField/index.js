fa868369b2ffc6c3c98a6d5cf688b28d7af27902
Gustavo Willemann <gustavowill@hotmail.com>
2021-04-01 17:26:19 -0300

    feat: projeto ao final da aula 06 modulo 04

--- src/components/forms/TextField/index.js
+++ src/components/forms/TextField/index.js
@@ -25,8 +25,12 @@ export default function TextField({
   name,
   onChange,
   value,
+  error,
+  isTouched,
   ...props
 }) {
+  const hasError = Boolean(error);
+  const isFieldInvalid = hasError && isTouched;
   return (
     <InputWrapper>
       <Input
@@ -38,13 +42,29 @@ export default function TextField({
         // eslint-disable-next-line react/jsx-props-no-spreading
         {...props}
       />
+      {isFieldInvalid && (
+        <Text
+          variant="smallestException"
+          color="error.main"
+          role="alert"
+        >
+          {error}
+        </Text>
+      )}
     </InputWrapper>
   );
 }
+
+TextField.defaultProps = {
+  error: undefined,
+  isTouched: undefined,
+};
 
 TextField.propTypes = {
   placeholder: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   value: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  isTouched: PropTypes.bool,
 };
