6e5381e8c1ccd6a9f996ef58684546dea98453f9
Gustavo Willemann <guswillemann@gmail.com>
2021-10-27 17:40:55 -0300

    feat(seo): adds meta tags

--- src/components/commons/SEO/index.js
+++ src/components/commons/SEO/index.js
@@ -4,34 +4,44 @@ import PropTypes from 'prop-types';
 
 export default function SEO({ headTitle }) {
   const hasHeadTitle = Boolean(headTitle);
-  const baseTitle = 'Instalura - Projeto Base do Alura Bootcamp JAMStack';
+  const baseTitle = 'Instalura';
   const title = hasHeadTitle
     ? (`${headTitle} | ${baseTitle}`)
     : baseTitle;
 
-  const description = 'Aprenda Programação, Front-end, Data Science, UX, DevOps, Marketing, Inovação e Gestão na maior plataforma de tecnologia do Brasil';
-  const image = 'https://www.alura.com.br/assets/img/alura-share.1571848411.png';
-  const urlBase = 'urldomeuprojeto.com.br';
+  const description = 'Compartilhe momentos e conecte-se com amigos';
+  const urlBase = 'https://instalura.guswillemann.vercel.app/';
+  const image = `${urlBase}images/meta-img.png`;
+  const imageAlt = 'Banner onde se lê: Instalura, Orgulhosamente criado durante o Bootcamp Alura JAM Stack';
 
   return (
     <Head>
       <title>{title}</title>
       <meta name="title" content={title} />
       <meta name="description" content={description} />
+      <link rel="icon" href="/images/favicon.svg" />
 
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content={urlBase} />
-      <meta property="og:title" content={title} />
+      <meta property="og:title" content={baseTitle} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
+
+      <meta property="og:image:alt" content={imageAlt} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="628" />
 
       {/* <!-- Twitter --> */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={urlBase} />
-      <meta property="twitter:title" content={title} />
+      <meta property="twitter:title" content={baseTitle} />
       <meta property="twitter:description" content={description} />
       <meta property="twitter:image" content={image} />
+
+      <meta property="twitter:image:alt" content={imageAlt} />
+      <meta property="twitter:image:width" content="1200" />
+      <meta property="twitter:image:height" content="628" />
     </Head>
   );
 }
